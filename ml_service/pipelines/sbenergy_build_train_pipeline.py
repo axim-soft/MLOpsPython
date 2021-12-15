9170d997c571e147076b2d7535be4a52aa6af61f
axim-soft <94515531+axim-soft@users.noreply.github.com>
2021-12-15 15:42:47 +0530

    Update sbenergy_build_train_pipeline.py

--- ml_service/pipelines/sbenergy_build_train_pipeline.py
+++ ml_service/pipelines/sbenergy_build_train_pipeline.py
@@ -65,7 +65,7 @@ def main():
         # create_sample_data_csv()
 
         # Use a CSV to read in the data set.
-        file_name = "juno_3month_one_hour.csv"
+        file_name = "diabetes.csv"
         
 
         if not os.path.exists(file_name):
